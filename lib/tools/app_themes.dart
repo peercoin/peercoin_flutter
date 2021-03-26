6bc02bfe9a8e594abe0501440320f8920b433807
Po <counterparty@outlook.com>
2021-03-26 15:44:53 +0100

    unused code removed

--- lib/tools/app_themes.dart
+++ lib/tools/app_themes.dart
@@ -37,14 +37,9 @@ class MyTheme{
   };
 
   static ThemeData getTheme(ThemeMode mode){
-    ThemeData x = appThemes[mode];
-   // x.primarySwatch: peercoinGreen;
-    return x;
+    return appThemes[mode];
   }
 }
-
-
-//appThemes[ThemeMode.light].
 
 abstract class LightThemeColors {
   static Color get background => const Color(0xFFFFFFFF);
@@ -57,42 +52,3 @@ abstract class DarkThemeColors {
   static Color get primaryContent => const Color(0xFFE1E1E1);
   static Color get primaryAccent => const Color(0xFFC7482A);
 }
-/*
-class ThemedColor {
-  final Color light;
-  final Color dark;
-
-  const ThemedColor({
-    @required this.light,
-    @required this.dark,
-  })  : assert(light != null),
-        assert(dark != null);
-
-  Color getColor(BuildContext context) {
-    switch (Theme.of(context).brightness) {
-      case Brightness.light:
-        return light;
-      case Brightness.dark:
-        return dark;
-    }
-    throw UnsupportedError('${Theme.of(context).brightness} is not supported');
-  }
-}*/
-/*
-abstract class AppColors {
-  static Color background(BuildContext context) => ThemedColor(
-    light: LightThemeColors.background,
-    dark: DarkThemeColors.background,
-  ).getColor(context);
-
-  static Color primaryContent(BuildContext context) => ThemedColor(
-    light: LightThemeColors.primaryContent,
-    dark: DarkThemeColors.primaryContent,
-  ).getColor(context);
-
-  static Color primaryAccent(BuildContext context) => ThemedColor(
-    light: LightThemeColors.primaryAccent,
-    dark: DarkThemeColors.primaryAccent,
-  ).getColor(context);
-}
-*/
