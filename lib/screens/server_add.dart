4cd370b98e17254507ff7c426d206d6c1fe14828
bananenwilly <pussinbootsdev@gmail.com>
2021-04-20 12:07:52 +0200

    scaffold skeleton

--- lib/screens/server_add.dart
+++ lib/screens/server_add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peercoin/tools/app_localizations.dart';
 
 class ServerAddScreen extends StatefulWidget {
   @override
@@ -8,6 +9,25 @@ class ServerAddScreen extends StatefulWidget {
 class _ServerAddScreenState extends State<ServerAddScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            AppLocalizations.instance.translate('server_add_title'),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: IconButton(
+              onPressed: () {
+                print("save");
+              },
+              icon: Icon(Icons.save),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
