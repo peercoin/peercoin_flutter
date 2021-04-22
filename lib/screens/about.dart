a23fb2dbaf6a02f808b753c1914c02eb3e18eb38
bananenwilly <willy@app.peercoin.net>
2021-04-22 13:38:55 +0200

    skeleton

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:peercoin/tools/app_localizations.dart';
+import 'package:peercoin/widgets/app_drawer.dart';
 
 class AboutScren extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.instance.translate('about'),
+        ),
+      ),
+      drawer: AppDrawer(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(children: []),
+        ),
+      ),
+    );
   }
 }
