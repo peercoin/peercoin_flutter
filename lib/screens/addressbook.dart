e60ff07ba3856e4053b28bf5b63ba28ec165185b
bananenwilly <willy@app.peercoin.net>
2021-04-30 18:36:52 +0200

    list receiving addresses, add 2dos

--- lib/screens/addressbook.dart
+++ lib/screens/addressbook.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:peercoin/models/walletaddress.dart';
+import 'package:peercoin/providers/activewallets.dart';
 import 'package:peercoin/tools/app_localizations.dart';
 import 'package:peercoin/widgets/loading_indicator.dart';
+import 'package:provider/provider.dart';
 
 class AddressBookScreen extends StatefulWidget {
   @override
@@ -11,16 +14,19 @@ class _AddressBookScreenState extends State<AddressBookScreen> {
   bool _initial = true;
   String _walletName;
   String _walletTitle;
+  List<WalletAddress> _walletAddresses = [];
 
   @override
-  void didChangeDependencies() {
+  void didChangeDependencies() async {
     if (_initial) {
-      setState(() {
-        _initial = false;
-      });
       final _args = ModalRoute.of(context).settings.arguments as Map;
       _walletName = _args["name"];
       _walletTitle = _args["title"];
+      _walletAddresses =
+          await context.watch<ActiveWallets>().getWalletAddresses(_walletName);
+      setState(() {
+        _initial = false;
+      });
     }
     super.didChangeDependencies();
   }
@@ -38,7 +44,45 @@ class _AddressBookScreenState extends State<AddressBookScreen> {
           AppLocalizations.instance
               .translate('addressbook_title', {"coin": _walletTitle}),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => print("searchin"),
+          ), //TODO implement
+        ],
       ),
+      body: _walletAddresses.isEmpty
+          ? Center(child: LoadingIndicator())
+          : ListView.builder(
+              //TODO add selection chips for incoming / outgoing
+              itemCount: _walletAddresses.length,
+              itemBuilder: (ctx, i) {
+                return Card(
+                  child: InkWell(
+                    onTap: () => print("this is how I win"),
+                    child: ListTile(
+                      title: Center(
+                        child: Text(_walletAddresses[i].address),
+                      ),
+                      subtitle: Center(
+                        child: Text(
+                          _walletAddresses[i].addressBookName ??
+                              AppLocalizations.instance
+                                  .translate('addressbook_no_label'),
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      //TODO add leading icon indicating direciton
+                    ),
+                  ),
+                );
+              }),
     );
   }
 }
+
+//TODO TX List show label if set
+//TODO send tab add label input (optional)
+//TODO receive tab add label input (optional)
