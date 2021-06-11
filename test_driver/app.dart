b095f21076b812635372c7443e517f01eb82f34b
bananenwilly <willy@app.peercoin.net>
2021-06-11 15:48:42 +0200

    remove comments

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -3,11 +3,8 @@ import 'package:peercoin/main.dart' as app;
 import 'package:flutter_driver/flutter_driver.dart';
 
 void main() {
-  // This line enables the extension.
   useMemoryFileSystemForTesting();
   enableFlutterDriverExtension();
 
-  // Call the `main()` function of the app, or call `runApp` with
-  // any widget you are interested in testing.
   app.main();
 }
