8663a3e2eb98096b43c86d42cd647ba0b1885dda
bananenwilly <pussinbootsdev@gmail.com>
2021-09-22 14:29:09 +0200

    v0.11 release preperation

--- lib/models/availablecoins.dart
+++ lib/models/availablecoins.dart
@@ -24,6 +24,7 @@ class AvailableCoins {
       explorerTxDetailUrl: 'https://blockbook.peercoin.net/tx/',
       genesisHash:
           '0000000032fe677166d54963b62a4677d8957e87c508eaa4fd7eb1c880cd27e3',
+      txVersion: 1, //TODO v0.11 main net release: change to 3
     ),
     'peercoinTestnet': Coin(
       name: 'peercoinTestnet',
@@ -46,6 +47,7 @@ class AvailableCoins {
       explorerTxDetailUrl: 'https://tblockbook.peercoin.net/tx/',
       genesisHash:
           '00000001f757bb737f6596503e17cd17b0658ce630cc727c0cca81aec47c9f06',
+      txVersion: 1, //TODO v0.11 main net release: change to 3
     ),
   };
 
