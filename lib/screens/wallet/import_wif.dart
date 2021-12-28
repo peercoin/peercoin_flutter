8934d68e3de02b9aa409d48dfde8a62f0a07d6ad
Willy <willy@coinerella.com>
2021-12-28 12:41:55 +0100

    subscribe to imported addr

--- lib/screens/wallet/import_wif.dart
+++ lib/screens/wallet/import_wif.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:peercoin/models/availablecoins.dart';
 import 'package:peercoin/models/coin.dart';
 import 'package:peercoin/providers/activewallets.dart';
+import 'package:peercoin/providers/electrumconnection.dart';
 import 'package:peercoin/tools/app_localizations.dart';
 import 'package:peercoin/tools/app_routes.dart';
 import 'package:peercoin/widgets/buttons.dart';
@@ -19,6 +20,7 @@ class _ImportWifScreenState extends State<ImportWifScreen> {
   late String _walletName;
   bool _initial = true;
   late ActiveWallets _activeWallets;
+  late ElectrumConnection _electrumConnection;
   final _wifGlobalKey = GlobalKey<FormState>();
   final _formKey = GlobalKey<FormState>();
   final _wifController = TextEditingController();
@@ -30,6 +32,7 @@ class _ImportWifScreenState extends State<ImportWifScreen> {
         _walletName = ModalRoute.of(context)!.settings.arguments as String;
         _activeCoin = AvailableCoins().getSpecificCoin(_walletName);
         _activeWallets = Provider.of<ActiveWallets>(context);
+        _electrumConnection = Provider.of<ElectrumConnection>(context);
         _initial = false;
       });
     }
@@ -57,51 +60,79 @@ class _ImportWifScreenState extends State<ImportWifScreen> {
   }
 
   Future<void> performImport(String wif, String address) async {
+    //write to wallet
     await _activeWallets.addAddressFromWif(_walletName, wif, address);
+
     //subscribe
-    //pop
+    _electrumConnection.subscribeToScriptHashes(
+      {
+        address: _activeWallets.getScriptHash(_walletName, address),
+      },
+    );
+
+    //pop import wif
+    Navigator.of(context).pop();
   }
 
   Future<void> triggerConfirmMessage(BuildContext ctx, String privKey) async {
     final publicAddress =
         Wallet.fromWIF(privKey, _activeCoin.networkType).address ?? '';
-    await showDialog(
-      context: ctx,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(
-            AppLocalizations.instance.translate('paperwallet_confirm_import'),
-            textAlign: TextAlign.center,
-          ),
-          content: Text(
-            AppLocalizations.instance.translate(
-              'import_wif_alert_content',
-              {'address': publicAddress},
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text(
-                AppLocalizations.instance
-                    .translate('server_settings_alert_cancel'),
-              ),
-            ),
-            TextButton(
-              onPressed: () async {
-                Navigator.pop(context);
-                await performImport(privKey, publicAddress);
-              },
-              child: Text(
-                AppLocalizations.instance.translate('import_button'),
-              ),
-            ),
-          ],
-        );
-      },
+
+    //check if that address is already in the list
+    final _walletAddresses =
+        await _activeWallets.getWalletAddresses(_walletName);
+    final _specificAddressResult = _walletAddresses.where(
+      (element) => element.address == publicAddress,
     );
+
+    if (_specificAddressResult.isNotEmpty) {
+      //we have that address already
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(
+          AppLocalizations.instance.translate('import_wif_error_snack'),
+          textAlign: TextAlign.center,
+        ),
+        duration: Duration(seconds: 3),
+      ));
+    } else {
+      await showDialog(
+        context: ctx,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(
+              AppLocalizations.instance.translate('paperwallet_confirm_import'),
+              textAlign: TextAlign.center,
+            ),
+            content: Text(
+              AppLocalizations.instance.translate(
+                'import_wif_alert_content',
+                {'address': publicAddress},
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  AppLocalizations.instance
+                      .translate('server_settings_alert_cancel'),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  Navigator.pop(context);
+                  await performImport(privKey, publicAddress);
+                },
+                child: Text(
+                  AppLocalizations.instance.translate('import_button'),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
@@ -133,13 +164,14 @@ class _ImportWifScreenState extends State<ImportWifScreen> {
                         if (value == null || value.isEmpty) {
                           return AppLocalizations.instance
                               .translate('import_wif_error_empty');
-                        } else if (validatePrivKey(value)) {
+                        }
+                        if (validatePrivKey(value)) {
                           return AppLocalizations.instance
                               .translate('import_wif_error_failed_parse');
-                        } else {
-                          triggerConfirmMessage(context, value);
-                          return null;
                         }
+
+                        triggerConfirmMessage(context, value);
+                        return null;
                       },
                       decoration: InputDecoration(
                         icon: Icon(
@@ -202,4 +234,3 @@ class _ImportWifScreenState extends State<ImportWifScreen> {
 
 //TODO add hint that key needs to be WIF format
 //TODO add hint that key has to be imported again when restoring the wallet from seed
-//TODO check if we have that addr already
