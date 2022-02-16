de1979d543f03a9616b923c39ab79fe54f68bd10
Willy <willy@coinerella.com>
2022-02-16 19:01:09 +0100

    Bug fix for importing paper wallets

--- lib/screens/wallet/import_paper_wallet.dart
+++ lib/screens/wallet/import_paper_wallet.dart
@@ -251,8 +251,14 @@ class _ImportPaperWalletScreenState extends State<ImportPaperWalletScreen> {
   }
 
   Future<void> buildImportTx([int fee = 0, bool dryRun = true]) async {
+    FlutterLogs.logInfo(
+      'ImportPaperWallet',
+      'buildImportTx',
+      'fee $fee - dryRun $dryRun',
+    );
+
     final tx = TransactionBuilder(network: _activeCoin.networkType);
-    tx.setVersion(1);
+    tx.setVersion(3);
     //send everything minus fees to unusedaddr
     tx.addOutput(_activeWallets.getUnusedAddress, _balanceInt - fee);
     //add inputs
@@ -272,6 +278,12 @@ class _ImportPaperWalletScreenState extends State<ImportPaperWalletScreen> {
         .toStringAsFixed(_activeCoin.fractions);
     var asDouble = double.parse(number) * 1000000;
     var requiredFeeInSatoshis = asDouble.toInt();
+
+    FlutterLogs.logInfo(
+      'ImportPaperWallet',
+      'buildImportTx',
+      'size ${intermediate.txSize}',
+    );
 
     if (dryRun == false) {
       _transactionHex = intermediate.toHex();
