4c07c040acd8fa9524824e91c8b1d6e121de58db
Willy <willy@coinerella.com>
2022-03-22 11:55:25 +0100

    list of reminder items

---
+++ lib/models/available_periodic_reminder_items.dart
@@ -0,0 +1,20 @@
+import 'package:peercoin/models/periodic_reminder_item.dart';
+
+class AvailablePeriodicReminderItems {
+  final Map<String, PeriodicReminderItem> availableReminderItems = {
+    'donate': PeriodicReminderItem(
+      id: 'donate',
+      title: 'periodic_reminder_donate_title', //TODO write text into en.json
+      body: 'periodic_reminder_donate_body',
+      button: 'periodic_reminder_donate_button',
+      buttonFunction: () => print('hi'),
+    ),
+    'backup': PeriodicReminderItem(
+      id: 'backup',
+      title: 'periodic_reminder_backup_title',
+      body: 'periodic_reminder_backup_body',
+      button: 'periodic_reminder_backup_button',
+      buttonFunction: () => print('hi'),
+    )
+  };
+}
--- lib/models/periodic_reminder_item.dart
+++ lib/models/periodic_reminder_item.dart
@@ -3,13 +3,15 @@ class PeriodicReminderItem {
   final String title;
   final String body;
   final String button;
-  DateTime _nextNotification = DateTime(1970);
-  DateTime _lastNotification = DateTime(1970);
+  final Function buttonFunction;
+  DateTime nextNotification = DateTime(1970);
+  DateTime lastNotification = DateTime(1970);
 
   PeriodicReminderItem({
     required this.id,
     required this.title,
     required this.body,
     required this.button,
+    required this.buttonFunction,
   });
 }
