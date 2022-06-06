500598809dc3718f6817f99a08de7c2c9c0a6d43
Willy <willy@coinerella.com>
2022-06-06 15:56:56 +0200

    Built web
    Branch: 0.9.3
    Commit: d6cea4425f28e67cd42edf75d32765b6b1d58da8
    
    package:peanut 4.2.0

---
+++ assets/CHANGELOG.md
@@ -0,0 +1,135 @@
+### **0.9.3** (2022-06-xx)
+* Signing messages is now available for each wallet
+* Improved error handling for edge case in which secure storage is not accessible on Android phones
+* Legal notices for setup
+* Various dependency updates under the hood
+
+### **0.9.2** (2022-05-13)
+* Improved wallet performance:  
+From now on the wallet will only watch addresses that it knows to have coins and the unused address (the one displayed in the "Receive" tab).   
+You can manually enable watching other addresses in the address book (slide left).   
+Background notifications only work for watched addresses.  
+Rescans are not affected. 
+
+### **0.9.1** (2022-05-12)
+* Price ticker: show latest price update
+* Minor localization improvements
+
+### **0.9.0** (2022-04-26)
+* Bug fix: Edge case for importing paper wallets
+
+### **0.8.9** (2022-04-14)
+* Remove +1 Satoshi extra fee
+* Better scanning and notifications 
+
+### **0.8.8** (2022-04-13)
+* Add language: Danish
+* Add currency: DKK
+* Scanning performance improvements
+* Bug fix: Sending OP_RETURN/Metadata messages working again
+* Bug fix: Edge case for unused addresses
+
+### **0.8.7** (2022-03-29)
+* Periodic backup and donation reminders
+* Add language: Swedish
+* Add currency: AUD, SEK
+
+### **0.8.6** (2022-03-09)
+* Add language: Filipino
+* Add currencies: PHP
+
+### **0.8.5** (2022-02-18)
+* Add language: Arabic
+* Add language: Japanese
+* Add language: Thai 
+* Add currencies: THB, JPY
+* Fix for Norwegian
+* Fix for importing paper wallets
+
+### **0.8.4** (2022-02-13)
+* Add language: Bangla 
+* Add language: Farsi
+* Add language: Hindi
+* Add language: Indonesian
+* Add language: Korean
+* Add language: Swahili
+* Add language: Ukrainian
+* Add language: Urdu
+* Add language: Turkish
+* Add language: Vietnamese
+* Fix for Norwegian
+* Better transaction building
+
+### **0.8.3** (2022-02-07)
+* Fix Android 12 crash
+* Add currencies: BDT, KRW, TRY
+
+### **0.8.2** (2022-02-04)
+* Add Norwegian to available languages
+* Completed Portoguese translation
+* Add NOK as available currency
+
+### **0.8.1** (2022-02-01)
+* Minor bug fixes and improvements under the hood
+
+### **0.8.0** (2022-01-27)
+* Address book - Your Addresses: allow to filter for change, empty or used addresses 
+* Address book - Your Addresses: allow to switch between addresses balance or label
+* App Settings: Allow to manually share debug logs
+
+### **0.7.9** (2022-01-11)
+* Allow keys to be exported in address book
+* Performance improvements for transaction building for imported keys
+
+### **0.7.8** (2022-01-06)
+* Translation fixes
+* Fix issue where wallet balance was not updated after TX confirmed
+* Fix issue with authentication 
+
+### **0.7.7** (2021-12-30)
+* allow import of WIF-format private keys
+* add Spanish translation 
+
+### **0.7.6** (2021-12-17)
+* fix issue with sending to P2WSH (Segwit Multisig) 
+
+### **0.7.5** (2021-12-16)
+* Allow sending to P2WSH (Segwit Multisig) 
+
+### **0.7.4** (2021-12-07)
+* Allow sending to P2SH (Multisig) 
+* Fix issue where inbound transactions would trigger unnecessary notifications 
+* Byte size will now be counted correctly when sending Metadata
+
+### **0.7.3** (2021-12-03)
+* Add PLN (Polish Zloty) to price feed
+* Add "Empty wallet" button to send tab
+* Allow to send 0 outputs when Metadata is present
+
+### **0.7.2** (2021-11-25)
+* Transaction Details: incoming and outgoing Metadata is now displayed
+* Fix for sending without Metadata (OP_RETURN)
+
+### **0.7.1** (2021-11-18)
+* OP_RETURN messages can now be included in the "send" tab of the wallet
+* French and Polish translation
+* Minor changes under the hood
+
+### **0.7.0** (2021-10-08)
+* Background notifications can now be enabled in app settings  
+Please perform a wallet scan to avoid unnecessary notifications. 
+* Transactions will now be send without timestamp (version 3, 0.11 hard fork)
+
+### **0.6.4** (2021-10-08)
+* Fix for older Android 10 devices
+* Fix for price data feed
+* Peercoin v0.11 hard fork preperation (Nov 1st 2021) 
+
+### **0.6.3** (2021-09-17)
+* New setup screens
+* Changelog screen after updates
+
+### **0.6.2** (2021-09-12)
+* Address book: enable double tap to clipboard & don't show fee for inbound tx 
+* Add fourth setup step: Enable price feed API
+* Allow import scan screen to be canceled
---
+++ flutter_service_worker.js
@@ -0,0 +1,239 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "version.json": "4e1dcbcad52dcd8270dd2683b0a5f359",
+"favicon.ico": "ad86c3b942b739addeee37dae114ade7",
+"index.html": "4dcc93b94b41f9864a31bd38869693d8",
+"/": "4dcc93b94b41f9864a31bd38869693d8",
+"main.dart.js": "3cd53a8146eeab33e63d3b9dc5082d11",
+"flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
+"icons/Icon-192.png": "d8ed4418b7e314bca062868e82748e9f",
+"icons/Icon-512.png": "2b839693a7a9604f2bde0cd67fd17abc",
+"manifest.json": "10413e652ec1368443e68e96fa0b876f",
+"assets/CHANGELOG.md": "9709e6897fb92bf01c80f37e5e1e1338",
+"assets/AssetManifest.json": "1d141979b24915ce4074a82a7c1c5981",
+"assets/NOTICES": "ea5c4eb560762b7e0875a5a9202d14e0",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/assets/img/setup-legal.png": "a5f0f4ec17060446a8f22f0aa143b3be",
+"assets/assets/img/setup-launch.png": "12fc61363b074ce7c51cb1f353433345",
+"assets/assets/img/setup-consent.png": "11a73f79fff6cdd2f64a78793dcd0c32",
+"assets/assets/img/setup-security.png": "0e38cf5b064c7559b1ca5f64fbd6e608",
+"assets/assets/img/setup-protection.png": "7d1b5c326c6d648364d48c4d4c30c15d",
+"assets/assets/img/list-empty.png": "0385368cc21600608a7f0c8ede129882",
+"assets/assets/icon/ppc-icon-256.png": "e5893fe49cc40e589885c69aa6e8a1ed",
+"assets/assets/icon/ppc-icon-48.png": "77a7f5e9cfab4965c9e356ade1087fde",
+"assets/assets/icon/ppc-logo.png": "d564432a5793345b243bb4afcefd2611",
+"assets/assets/icon/ppc-icon-white-48.png": "4cc5c9b1858468267167e5a9ec3ff62d",
+"assets/assets/icon/ppc-icon-white-256.png": "817576d10026737e3423c39c05e1eed1",
+"assets/assets/icon/ppc-ios-app-xxl.png": "457ede70c42d9f8acc53d9e3c91137a2",
+"assets/assets/icon/ppc-icon.png": "1614470da6cab032cc9afd1bad8ddbb3",
+"assets/assets/icon/ppc-icon-white-bg.png": "60d7216eaee21cc15e50b2cea455a0c3",
+"assets/assets/translations/zh.json": "d456ba009d4f52004cd0ffcb6a30d866",
+"assets/assets/translations/tr.json": "e62f83f81de693b7d9fd1d552f47d989",
+"assets/assets/translations/ha.json": "67e3024d6acc599309e577a72ebfaf5c",
+"assets/assets/translations/nl.json": "6fa553a56049131b1a097d0e31432d2f",
+"assets/assets/translations/ja.json": "7abe2ff098d40331b6cd5ae0dd153a2b",
+"assets/assets/translations/de.json": "3c552f7e3953d42664663cbabb17580c",
+"assets/assets/translations/ru.json": "85f19aeac6ab75d9992a61a6cd2fa18b",
+"assets/assets/translations/pl.json": "3f439431cad3318020514d85698e08aa",
+"assets/assets/translations/uk.json": "ee312211d2325047d469e070f02df682",
+"assets/assets/translations/fil.json": "8706328023f9daa026ba8ac475853ee2",
+"assets/assets/translations/ur.json": "0c2cfee944f268f9d34d6f25f18ac2f6",
+"assets/assets/translations/pt.json": "4d220408efadd49430afa5ccfe75c060",
+"assets/assets/translations/en.json": "97cc4e143d7d496bb52de3822970eff6",
+"assets/assets/translations/it.json": "955b93adcbd0bec088e0602d64d571a6",
+"assets/assets/translations/hr.json": "716a8b977480dff660ce72921dfdd01a",
+"assets/assets/translations/fr.json": "e25df98f59c5d29f98beacce7462b3ec",
+"assets/assets/translations/el.json": "7b610e956700ed0d2e7fa177548dc3d0",
+"assets/assets/translations/ro.json": "822a6bf57885e5eea47b50b7a75afb21",
+"assets/assets/translations/hi.json": "e5c0ef75e427cef1225bfd93bbd9fdc6",
+"assets/assets/translations/ko.json": "9545485c8451e682f66e420adb821fd7",
+"assets/assets/translations/vi.json": "24e942c75c0042679b4c132c5e06aa9f",
+"assets/assets/translations/nb_NO.json": "f64e9ba86b4a572669e8a1adb35b55dc",
+"assets/assets/translations/fa.json": "8b8035df0acae80f5b1757103af0a378",
+"assets/assets/translations/id.json": "f7b0fa02f02a59d3800744e8b21df016",
+"assets/assets/translations/bn_BD.json": "1374f74e4efd641adf8ca6fddbff6a2d",
+"assets/assets/translations/sw.json": "236a11b1a0d506feef4f04f9c60f996c",
+"assets/assets/translations/da.json": "59abf84b67f85c779bbdb572075195e9",
+"assets/assets/translations/th.json": "11ac8c4c03e752b1dc71f19b5ecedd4a",
+"assets/assets/translations/sv.json": "40d90e692671910b613cbb5f6f4518c9",
+"assets/assets/translations/es.json": "64576f29c288c7ac63f43eb5c571f335",
+"assets/assets/translations/ar.json": "6e721d279114fe3828047d716df55d57",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "main.dart.js",
+"index.html",
+"assets/NOTICES",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
