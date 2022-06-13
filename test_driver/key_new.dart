b5f15be5b1d89f7f1731152e75aeb3356c041759
Willy <willy@coinerella.com>
2022-06-14 01:22:56 +0200

    update e2e

--- test_driver/key_new.dart
+++ test_driver/key_new.dart
@@ -38,46 +38,31 @@ void main() {
         () async {
           //creates a brand new peercoin testnet wallet from scratch and check if it connects
           await driver.tap(find.byValueKey('setupLanguageButton'));
-          print('found setupLanguageButton');
           await driver.tap(find.text('English'));
-          print('found English');
           await driver.tap(find.pageBack());
-          print('found pageBack');
           await driver.tap(find.text('Create Wallet'));
-          print('found Create Wallet');
           await driver.tap(find.text('Export now'));
-          print('found Export now');
           await driver.tap(find.text('Continue'));
-          print('found Continue');
           await driver.tap(find.text('Continue'));
-          print('found Continue');
           await driver.tap(elevatedButtonFinder); //pin pad
           for (var i = 1; i <= 12; i++) {
-            print('tap 0');
             await driver.tap(find.text('0'));
           }
           await driver.tap(find.byValueKey('setupApiTickerSwitchKey'));
-          print('found setupApiTickerSwitchKey');
           await driver.tap(find.byValueKey('setupApiBGSwitchKey'));
-          print('found setupApiBGSwitchKey');
           await driver.tap(find.text('Continue'));
-          print('found Continue');
-          final pixels = await driver.screenshot();
-          final file = File('shot.png');
-          await file.writeAsBytes(pixels);
-          await driver.tap(find.byType('SwitchListTile'));
-          print('found SwitchListTile');
+          // final pixels = await driver.screenshot();
+          // final file = File('shot.png');
+          // await file.writeAsBytes(pixels);
+          await driver.tap(find.byValueKey('setupLegalConsentKey'));
           await driver.tap(find.text('Finish Setup'));
-          print('found Finish Setup');
           await driver.tap(find.pageBack());
-          print('found pageBack');
           await driver.runUnsynchronized(
             () async {
               expect(
                 await driver.getText(find.byValueKey('noActiveWallets')),
                 'You have no active wallets',
               );
-              print('found noActiveWallets');
             },
           );
         },
