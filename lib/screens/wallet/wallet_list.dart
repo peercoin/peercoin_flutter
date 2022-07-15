4d5bd6faaea47608eb99bae3a076c2732790a980
Willy <willy@coinerella.com>
2022-07-15 19:23:16 +0200

    praise the linter

--- lib/screens/wallet/wallet_list.dart
+++ lib/screens/wallet/wallet_list.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
