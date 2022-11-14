ba7ca96267480796d356911af5c157f2e0ad36c8
Willy <willy@coinerella.com>
2022-11-14 16:44:30 +0100

    pragma

--- lib/tools/background_sync.dart
+++ lib/tools/background_sync.dart
@@ -24,6 +24,7 @@ import 'logger_wrapper.dart';
 import 'notification.dart';
 
 class BackgroundSync {
+  @pragma('vm:entry-point')
   static void backgroundFetchHeadlessTask(HeadlessTask task) async {
     var taskId = task.taskId;
     var isTimeout = task.timeout;
