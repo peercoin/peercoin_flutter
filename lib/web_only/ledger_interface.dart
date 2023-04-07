322927e8ef97b40321ecf548f78ea9b50c30e53e
Willy <willy@coinerella.com>
2023-04-07 16:56:33 +0200

    comments

--- lib/web_only/ledger_interface.dart
+++ lib/web_only/ledger_interface.dart
@@ -3,13 +3,16 @@ library ledger_interface;
 
 import 'package:js/js.dart';
 
+// creates a new transport object (Promise)
 @JS('TransportWebUSB.create')
 external Object transportWebUSBCreate();
 
+// creates a new Btc object (Promise)
 @JS('Btc')
 class Btc {
   external Btc(Object transport);
   external Object getWalletPublicKey(
+    // returns a Promise
     String path,
     Options options,
   );
