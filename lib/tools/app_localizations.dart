fbda1587cf4524c64627b7d52e084e93c1d6396c
Willy <willy@coinerella.com>
2023-08-02 16:16:17 +0200

    Add language: Kirghiz / Kyrgyz

--- lib/tools/app_localizations.dart
+++ lib/tools/app_localizations.dart
@@ -59,6 +59,7 @@ class AppLocalizations {
     'ko': (Locale('ko'), '한국어 [韓國語] (han-guk-eo)'),
     'km': (Locale('km'), 'ភាសាខ្មែរ (phéasa khmae)'),
     'kn': (Locale('kn'), 'ಕನ್ನಡ (kannaḍa)'),
+    'ky': (Locale('ky'), 'Кыргызча (Kyrgyzcha)'),
     'lt': (Locale('lt'), 'Lietuvių kalba'),
     'hu': (Locale('hu'), 'Magyar'),
     'hy': (Locale('hy'), 'Հայերեն (Hayeren)'),
