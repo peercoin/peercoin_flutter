8b1d884682d9516ac1abf41b70db81a5bb5d1361
Willy <willy@coinerella.com>
2024-07-05 17:24:52 +0200

    feat: Add loader overlay to FrostWalletHomeScreen

--- lib/screens/wallet/frost_wallet_home.dart
+++ lib/screens/wallet/frost_wallet_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class FrostWalletHomeScreen extends StatefulWidget {
   const FrostWalletHomeScreen({super.key});
@@ -8,6 +9,20 @@ class FrostWalletHomeScreen extends StatefulWidget {
 }
 
 class _FrostWalletHomeScreenState extends State<FrostWalletHomeScreen> {
+  bool _initial = true;
+
+  @override
+  void didChangeDependencies() {
+    if (_initial) {
+      context.loaderOverlay.hide();
+      setState(() {
+        _initial = false;
+      });
+    }
+
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
