8289188f54f4e0931486f7c03ccef47603bf9a34
Willy <willy@coinerella.com>
2024-07-10 18:08:57 +0200

    feat: Update FrostGroup model and adapter

--- lib/models/hive/frost_group.dart
+++ lib/models/hive/frost_group.dart
@@ -1,5 +1,41 @@
 import 'package:hive/hive.dart';
+import 'package:frost_noosphere/frost_noosphere.dart';
 part 'frost_group.g.dart';
 
 @HiveType(typeId: 8)
-class FrostGroup extends HiveObject {}
+class FrostGroup extends HiveObject {
+  @HiveField(0)
+  final String _name;
+
+  @HiveField(1)
+  bool _isCompleted = false;
+
+  @HiveField(2)
+  ClientConfig _clientConfig;
+
+  @HiveField(3)
+  String _serverUrl;
+
+  FrostGroup(
+      this._name, this._isCompleted, this._clientConfig, this._serverUrl);
+
+  String get name => _name;
+
+  bool get isCompleted => _isCompleted;
+  set isCompleted(bool value) {
+    _isCompleted = value;
+    save();
+  }
+
+  ClientConfig get clientConfig => _clientConfig;
+  set clientConfig(ClientConfig value) {
+    _clientConfig = value;
+    save();
+  }
+
+  String get serverUrl => _serverUrl;
+  set serverUrl(String value) {
+    _serverUrl = value;
+    save();
+  }
+}
