292aaad92648dca8ae70a48c59fe1847dbc4a334
Willy <willy@coinerella.com>
2024-07-26 18:04:09 +0200

    Refactor FrostGroup setup landing page
    The FrostGroup setup landing page has been refactored to improve the user experience. Changes include updating the layout, adding form inputs for group name and group ID, and including a server URL input. Additionally, the success message has been updated to inform users that they will be taken to the next page where the public keys of the participants will be shown.
    
    Note: The group name can now be changed later through a context menu to avoid confusion between the group name and group ID.

--- lib/widgets/wallet/frost_group/setup_landing.dart
+++ lib/widgets/wallet/frost_group/setup_landing.dart
@@ -173,10 +173,8 @@ class _FrostGroupSetupLandingState extends State<FrostGroupSetupLanding> {
 
 // TODO group name can be changed later through context menu to avoid confusion between group name and group id
 
-// 3. If the configuration is not complete, there will be a configuration page that displays a public key for the participant for them to share.
+
 // 4. On this page a list of other participants is shown with: 1. Name. 2. Identifier and 3. Public key. This list will have a "+" or "Add" button to add a new participant with the ability to edit or remove other participant details.
-// 5. There will also be an ID field for the group that can be any string. Should we limit to alphanumeric and possibly underscores?
 // 6. After details are updated a new potential GroupConfig will be created and the fingerprint will be shown so that it can be compared against other participant's config to ensure it is the same.
-// 7. If a user navigates away from the screen, the current configuration settings will be saved in an incomplete state.
 // 7. A "Finish" button will move the state to a completed configuration. An ability to download the configuration details for use on a coordination server will be needed.
 // 8. Perhaps the next page should have the options: 1. Connect to server (which will present the full DKG and signing options later). 2. Download configuration (for use on a server). 3. Modify configuration (if configuration is later changed, it goes back to the previous screen).
