3c7799f5958c56c9b4c07e87b17b8d0c050a4824
Willy <willy@coinerella.com>
2024-07-26 18:47:10 +0200

    Refactor FrostGroup setup landing page
    Improve user experience by updating layout, adding form inputs for group name and group ID, and including a server URL input. Update success message to inform users about the next page where public keys of participants will be shown. Allow changing group name through context menu to avoid confusion with group ID.

--- lib/widgets/wallet/frost_group/setup_pubkey.dart
+++ lib/widgets/wallet/frost_group/setup_pubkey.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:peercoin/tools/app_localizations.dart';
 import 'package:peercoin/widgets/buttons.dart';
 import 'package:peercoin/widgets/service_container.dart';
+import 'package:peercoin/widgets/wallet/frost_group/setup_landing.dart';
 
 class FrostGroupSetupPubkey extends StatefulWidget {
-  const FrostGroupSetupPubkey({super.key});
+  final Function changeStep;
+  const FrostGroupSetupPubkey({required this.changeStep, super.key});
 
   @override
   State<FrostGroupSetupPubkey> createState() => _FrostGroupSetupPubkeyState();
 }
 
 class _FrostGroupSetupPubkeyState extends State<FrostGroupSetupPubkey> {
+  void _showFingerprint() {
+    // TODO show fingerprint
+    print('hello');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -21,19 +29,59 @@ class _FrostGroupSetupPubkeyState extends State<FrostGroupSetupPubkey> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text('I am the setup page title'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            widget.changeStep(FrostSetupStep.group);
+                          },
+                          icon: const Icon(
+                            Icons.arrow_back_ios,
+                          ),
+                        ),
+                        Text(
+                          AppLocalizations.instance
+                              .translate('frost_setup_group_title'),
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     Text(
-                        'I am the description of the setup page and will tell you what is going on'),
+                      AppLocalizations.instance
+                          .translate('frost_setup_group_description'),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
                     Text(
-                        'I am the form Input for the group name (name it what you want)'),
-                    Text(
-                        'I am the form Input for the group id (carefull, this has to match the other participants)'),
-                    Text('I am the server url input'),
+                      AppLocalizations.instance
+                          .translate('frost_setup_group_hint'),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text('cards'),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     PeerButton(
-                        text: 'Save & Try connection',
-                        action: () => print('Save')),
-                    Text(
-                        'On success you will be taken to the next page where the public keys of the participants will be shown'),
+                      text: AppLocalizations.instance.translate(
+                        'frost_setup_group_cta',
+                      ),
+                      action: () => _showFingerprint(),
+                    ),
                   ],
                 ),
               ),
@@ -44,3 +92,7 @@ class _FrostGroupSetupPubkeyState extends State<FrostGroupSetupPubkey> {
     );
   }
 }
+// 4. On this page a list of other participants is shown with: 1. Name. 2. Identifier and 3. Public key. This list will have a "+" or "Add" button to add a new participant with the ability to edit or remove other participant details.
+// 6. After details are updated a new potential GroupConfig will be created and the fingerprint will be shown so that it can be compared against other participant's config to ensure it is the same.
+// 7. A "Finish" button will move the state to a completed configuration. An ability to download the configuration details for use on a coordination server will be needed.
+// 8. Perhaps the next page should have the options: 1. Connect to server (which will present the full DKG and signing options later). 2. Download configuration (for use on a server). 3. Modify configuration (if configuration is later changed, it goes back to the previous screen).
