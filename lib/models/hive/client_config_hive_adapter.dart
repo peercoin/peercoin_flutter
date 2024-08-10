dfdad1bf0a2c87d1b83246a0724cbb0208e49dca
Willy <willy@coinerella.com>
2024-08-10 15:46:34 +0200

    Refactor HiveFrostClientConfigAdapter to use hex encoding for ClientConfig serialization

--- lib/models/hive/client_config_hive_adapter.dart
+++ lib/models/hive/client_config_hive_adapter.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:frost_noosphere/frost_noosphere.dart';
 import 'package:hive/hive.dart';
 
@@ -10,21 +7,11 @@ class HiveFrostClientConfigAdapter extends TypeAdapter<ClientConfig> {
 
   @override
   ClientConfig read(BinaryReader reader) {
-    final json = reader.readString();
-    return ClientConfig.fromBytes(
-      Uint8List.fromList(
-        List<int>.from(
-          jsonDecode(
-            json,
-          ),
-        ),
-      ),
-    );
+    return ClientConfig.fromHex(reader.readString());
   }
 
   @override
   void write(BinaryWriter writer, ClientConfig obj) {
-    final json = jsonEncode(obj.toBytes());
-    writer.writeString(json);
+    writer.writeString(obj.toHex());
   }
 }
