3ab898bf8363b04f42a74666893110993e94c100
Willy <willy@coinerella.com>
2024-08-21 23:00:37 +0200

    skeleton for configured group

--- lib/widgets/wallet/frost_group/landing_configured.dart
+++ lib/widgets/wallet/frost_group/landing_configured.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peercoin/widgets/service_container.dart';
 
 class FrostGroupLandingConfigured extends StatefulWidget {
   const FrostGroupLandingConfigured({super.key});
@@ -12,6 +13,20 @@ class _FrostGroupLandingConfiguredState
     extends State<FrostGroupLandingConfigured> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(children: [
+      Expanded(
+        child: SingleChildScrollView(
+          child: Align(
+            child: PeerContainer(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [Text('hi')],
+              ),
+            ),
+          ),
+        ),
+      )
+    ]);
   }
 }
+// 8. Perhaps the next page should have the options: 1. Connect to server (which will present the full DKG and signing options later). 2. Download configuration (for use on a server). 3. Modify configuration (if configuration is later changed, it goes back to the previous screen).
