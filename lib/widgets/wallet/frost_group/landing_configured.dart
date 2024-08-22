7e58cca943dee7a77a1c618803675c4e9e9a704f
Willy <willy@coinerella.com>
2024-08-22 22:37:07 +0200

    layout

--- lib/widgets/wallet/frost_group/landing_configured.dart
+++ lib/widgets/wallet/frost_group/landing_configured.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peercoin/widgets/buttons.dart';
 import 'package:peercoin/widgets/service_container.dart';
 
 class FrostGroupLandingConfigured extends StatefulWidget {
@@ -13,20 +14,35 @@ class _FrostGroupLandingConfiguredState
     extends State<FrostGroupLandingConfigured> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Expanded(
-        child: SingleChildScrollView(
-          child: Align(
-            child: PeerContainer(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [Text('hi')],
+    return Column(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Align(
+              child: PeerContainer(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    PeerButton(
+                        text: 'Connect to server',
+                        action: () => print('Connect to Server')),
+                    const SizedBox(height: 20),
+                    PeerButton(
+                        text: 'Download configuration',
+                        action: () => print('Download configuration')),
+                    const SizedBox(height: 20),
+                    PeerButton(
+                      text: 'Modify configuration',
+                      action: () => print('Modify configuration'),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      )
-    ]);
+        )
+      ],
+    );
   }
 }
 // 1. Connect to server (which will present the full DKG and signing options later). 
