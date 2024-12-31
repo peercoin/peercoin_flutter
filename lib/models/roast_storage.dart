a1a35cce3ae68c7d2f0ad05faa94470494075485
Willy <willy@coinerella.com>
2024-12-31 12:38:11 +0100

    Implement methods for managing Frost keys and signatures in ROASTStorage

--- lib/models/roast_storage.dart
+++ lib/models/roast_storage.dart
@@ -6,46 +6,54 @@ class ROASTStorage implements ClientStorageInterface {
   ROASTStorage(this.roastGroup);
 
   @override
-  Future<void> addAck(SignedDkgAck ack) {
-    // TODO: implement addAck
-    throw UnimplementedError();
+  Future<void> addAck(SignedDkgAck ack) async {
+    final groupKey = ack.signed.obj.groupKey;
+    final key = roastGroup.keys[groupKey]!;
+
+    roastGroup.keys[groupKey] = FrostKeyWithDetails(
+      keyInfo: key.keyInfo,
+      name: key.name,
+      description: key.description,
+      acks: {...key.acks, ack},
+    );
   }
 
   @override
-  Future<void> addNewFrostKey(FrostKeyWithDetails newKey) {
-    // TODO: implement addNewFrostKey
-    throw UnimplementedError();
+  Future<void> addNewFrostKey(FrostKeyWithDetails newKey) async {
+    roastGroup.keys[newKey.groupKey] = newKey;
   }
 
   @override
   Future<void> addRejectedSigsRequest(
-      SignaturesRequestId id, FinalExpirable expirable) {
-    // TODO: implement addRejectedSigsRequest
-    throw UnimplementedError();
+    SignaturesRequestId id,
+    FinalExpirable expirable,
+  ) async {
+    roastGroup.sigsRejected[id] = expirable;
   }
 
   @override
   Future<void> addSignaturesNonces(
-      SignaturesRequestId id, SignaturesNonces nonces) {
-    // TODO: implement addSignaturesNonces
-    throw UnimplementedError();
+    SignaturesRequestId id,
+    SignaturesNonces nonces,
+  ) async {
+    final sigNonces = roastGroup.sigNonces;
+
+    if (sigNonces.containsKey(id)) {
+      sigNonces[id]!.map.addEntries(nonces.map.entries);
+    } else {
+      sigNonces[id] = nonces;
+    }
   }
 
   @override
-  Future<Set<FrostKeyWithDetails>> loadKeys() {
-    // TODO: implement loadKeys
-    throw UnimplementedError();
-  }
+  Future<Set<FrostKeyWithDetails>> loadKeys() async =>
+      roastGroup.keys.values.toSet();
 
   @override
-  Future<Map<SignaturesRequestId, FinalExpirable>> loadRejectedSigsRequests() {
-    // TODO: implement loadRejectedSigsRequests
-    throw UnimplementedError();
-  }
+  Future<Map<SignaturesRequestId, FinalExpirable>>
+      loadRejectedSigsRequests() async => roastGroup.sigsRejected;
 
   @override
-  Future<Map<SignaturesRequestId, SignaturesNonces>> loadSigNonces() {
-    // TODO: implement loadSigNonces
-    throw UnimplementedError();
-  }
+  Future<Map<SignaturesRequestId, SignaturesNonces>> loadSigNonces() async =>
+      roastGroup.sigNonces;
 }
