2b17013b390e7ebe9bacfe2d212c9489aceffa3f
Willy <willy@coinerella.com>
2025-02-24 18:19:32 +0100

    Implement ROAST wallet dashboard layout and add logout functionality

--- lib/screens/wallet/roast/roast_wallet_dasbhoard.dart
+++ lib/screens/wallet/roast/roast_wallet_dasbhoard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frost_noosphere/frost_noosphere.dart';
+import 'package:peercoin/widgets/buttons.dart';
+import 'package:peercoin/widgets/service_container.dart';
 
 class ROASTWalletDashboardScreen extends StatefulWidget {
   const ROASTWalletDashboardScreen({super.key});
@@ -33,7 +35,45 @@ class _ROASTWalletDashboardScreenState
   }
 
   @override
+  void dispose() {
+    _roastClient.logout();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ROAST Wallet Dashboard'), // TODO i18n
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Align(
+                child: PeerContainer(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      PeerButton(
+                        text: 'click',
+                        action: () => _roastClient.requestDkg(
+                          NewDkgDetails(
+                            name: 'test',
+                            description: 'test',
+                            threshold: 1,
+                            expiry: Expiry(const Duration(days: 1)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
