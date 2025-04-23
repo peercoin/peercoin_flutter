cd428115174547f8f09d5cc515392a04c9b011a7
Willy <willy@coinerella.com>
2025-04-23 18:53:59 +0200

    feat: update open requests display message for clarity in ROAST wallet

--- lib/widgets/wallet/roast_group/tabs/open_request_tab.dart
+++ lib/widgets/wallet/roast_group/tabs/open_request_tab.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:noosphere_roast_client/noosphere_roast_client.dart';
 import 'package:peercoin/tools/app_localizations.dart';
-import 'package:peercoin/widgets/service_container.dart';
 
 class OpenRequestTab extends StatelessWidget {
   final Client roastClient;
@@ -20,60 +19,77 @@ class OpenRequestTab extends StatelessWidget {
         ListView(
           children: [
             Align(
-              child: PeerContainer(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    roastClient.dkgRequests.isEmpty
-                        ? Text(
-                            AppLocalizations.instance.translate(
-                              'roast_wallet_open_requests_empty',
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  roastClient.dkgRequests.isEmpty
+                      ? Text(
+                          AppLocalizations.instance.translate(
+                            'roast_wallet_open_requests_empty',
+                          ),
+                        )
+                      : const SizedBox(),
+                  ...roastClient.dkgRequests.asMap().entries.map((entry) {
+                    final request = entry.value;
+                    return Card(
+                      elevation: 0,
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 8,
+                        horizontal: 16,
+                      ),
+                      color: Theme.of(context).colorScheme.surface,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 16,
+                          horizontal: 8,
+                        ),
+                        child: Column(
+                          children: [
+                            Text(
+                              request.details.name,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
                             ),
-                          )
-                        : const SizedBox(),
-                    ...roastClient.dkgRequests.asMap().entries.map((entry) {
-                      final index = entry.key;
-                      final request = entry.value;
-                      return Column(
-                        children: [
-                          index > 0 ? const Divider() : const SizedBox(),
-                          Text('Name: ${request.details.name}'),
-                          Text(
-                            'Description: ${request.details.description}',
-                          ),
-                          Text('Threshold: ${request.details.threshold}'),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              IconButton(
-                                icon: Icon(
-                                  Icons.check,
-                                  color: Theme.of(context).colorScheme.primary,
+                            Text(
+                              'Description: ${request.details.description}',
+                            ),
+                            Text('Threshold: ${request.details.threshold}'),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.check,
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                  ),
+                                  onPressed: () async {
+                                    await roastClient
+                                        .acceptDkg(request.details.name);
+                                    forceRender();
+                                  },
                                 ),
-                                onPressed: () async {
-                                  await roastClient
-                                      .acceptDkg(request.details.name);
-                                  forceRender();
-                                },
-                              ),
-                              IconButton(
-                                icon: Icon(
-                                  Icons.delete_forever,
-                                  color: Theme.of(context).colorScheme.error,
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.cancel,
+                                    color: Theme.of(context).colorScheme.error,
+                                  ),
+                                  onPressed: () async {
+                                    await roastClient
+                                        .acceptDkg(request.details.name);
+                                    forceRender();
+                                  },
                                 ),
-                                onPressed: () async {
-                                  await roastClient
-                                      .acceptDkg(request.details.name);
-                                  forceRender();
-                                },
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    }),
-                  ],
-                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                ],
               ),
             ),
           ],
