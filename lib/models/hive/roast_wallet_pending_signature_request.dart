91b82066e0a9fd67c07d696b6b654ae813e9a55b
Willy <willy@coinerella.com>
2025-05-05 17:38:08 +0200

    fix: remove unused message field from ROASTWalletPendingSignatureRequest model

--- lib/models/hive/roast_wallet_pending_signature_request.dart
+++ lib/models/hive/roast_wallet_pending_signature_request.dart
@@ -1,14 +1,12 @@
 import 'package:coinlib_flutter/coinlib_flutter.dart';
 
 class ROASTWalletPendingSignatureRequest {
-  String message;
   String despositAddress;
   String depositAmount;
   List<int> derivationPath;
   ECCompressedPublicKey selectedGroupKey;
 
   ROASTWalletPendingSignatureRequest({
-    required this.message,
     required this.despositAddress,
     required this.depositAmount,
     required this.selectedGroupKey,
