0db3da2983339e07e486e9885d7bb70dd09b848d
Willy <willy@coinerella.com>
2025-05-12 18:37:39 +0200

    feat: implement taproot transaction final assembly function

---
+++ lib/tools/taproot_transaction_final_assembly.dart
@@ -0,0 +1,8 @@
+import 'package:coinlib_flutter/coinlib_flutter.dart';
+import 'package:noosphere_roast_client/noosphere_roast_client.dart';
+
+Future<Transaction> taprootTransactionFinalAssembly(
+  SignaturesCompleteClientEvent event,
+) async {
+  return Transaction.fromHex('asdf'); //TODO
+}
