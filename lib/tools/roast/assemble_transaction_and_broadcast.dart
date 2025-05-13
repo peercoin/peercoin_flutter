aa6d52838a88e4694fce41042af0bd6654f82a77
Willy <willy@coinerella.com>
2025-05-13 18:52:43 +0200

    feat: add success and failure snack bar messages for transaction broadcasting

--- lib/tools/roast/assemble_transaction_and_broadcast.dart
+++ lib/tools/roast/assemble_transaction_and_broadcast.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:noosphere_roast_client/noosphere_roast_client.dart';
 import 'package:peercoin/generated/marisma.pbgrpc.dart';
+import 'package:peercoin/tools/app_localizations.dart';
 import 'package:peercoin/tools/logger_wrapper.dart';
 import 'package:peercoin/tools/roast/taproot_transaction_final_assembly.dart';
 
@@ -9,17 +10,24 @@ Future<void> assembleTransactionAndBroadcast(
   MarismaClient marismaClient,
   SignaturesCompleteClientEvent event,
 ) async {
+  String? hashHex;
+  bool isError = false;
+
   try {
     final builtTx = await taprootTransactionFinalAssembly(
       event,
     );
+    final txHex = builtTx.toHex();
+    hashHex = builtTx.hashHex;
+
     LoggerWrapper.logInfo(
       'ROASTWalletHomeScreen',
       'eventStream',
-      'Broadcasting transaction: ${builtTx.hashHex} - ${builtTx.toHex()} ',
+      'Broadcasting transaction: $hashHex - $txHex',
     );
+
     final res = await marismaClient.broadCastTransaction(
-      BroadCastTransactionRequest(hex: builtTx.toHex()),
+      BroadCastTransactionRequest(hex: txHex),
     );
     if (res.rpcError.isNotEmpty) {
       LoggerWrapper.logError(
@@ -27,13 +35,13 @@ Future<void> assembleTransactionAndBroadcast(
         'eventStream',
         'Failed to broadcast transaction: ${res.rpcError}',
       );
+      isError = true;
     } else {
       LoggerWrapper.logInfo(
         'ROASTWalletHomeScreen',
         'eventStream',
-        'Transaction broadcasted successfully: ${builtTx.hashHex}',
+        'Transaction broadcasted successfully: $hashHex',
       );
-      // TODO show success snackbar
 
       // TODO remove from sigNonces?1
     }
@@ -43,5 +51,23 @@ Future<void> assembleTransactionAndBroadcast(
       'eventStream',
       'Failed to broadcast transaction: $e',
     );
+    isError = true;
+  } finally {
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            AppLocalizations.instance.translate(
+              isError == false
+                  ? 'assemble_transaction_an_broadcast_success_snack'
+                  : 'assemble_transaction_an_broadcast_failed_snack',
+              {
+                'hashHex': hashHex ?? '',
+              },
+            ),
+          ),
+        ),
+      );
+    }
   }
 }
